80c99e69795db9c36a30682e0faf2ad2cf530cf9
Juan Fernandez <Juan.Fernandez@lpt.com>
2026-03-06 14:57:18 -0500

    vite config url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command }) => ({
-    plugins: [vue()],
-    base: process.env.VITE_BASE_PATH || (command === 'build' ? '/CSV-Converter/' : '/'),
-}));
+export default defineConfig({
+  plugins: [vue()],
+  base: '/CSV-Converter/',
+})
